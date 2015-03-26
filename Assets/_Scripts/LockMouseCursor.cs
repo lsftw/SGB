ead6a4ecc9fa6c4e13a46475070a4c6b85422400
Shiliang Zhang <lsftw13@gmail.com>
2015-03-25 23:49:38 -0400

    Add mouse cursor lock script.

---
+++ Assets/_Scripts/LockMouseCursor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockMouseCursor : MonoBehaviour {
+	public bool lockCursorAtStart = true;
+
+	// Use this for initialization
+	void Start () {
+		if (lockCursorAtStart) {
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+//			Screen.lockCursor = false;
+		} else {
+			Cursor.lockState = CursorLockMode.Locked;
+//			Screen.lockCursor = true;
+		}
+		bool cursorNormal = Cursor.lockState == CursorLockMode.None;
+		Cursor.visible = cursorNormal;
+	}
+}
