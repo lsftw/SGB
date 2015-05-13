8778ba727a73660ab532d3f10354e7cb6fc838e6
Shiliang Zhang <lsftw13@gmail.com>
2015-05-12 21:26:00 -0400

    Fixed projectile not moving bug.

--- Assets/_Scripts/Code425.cs
+++ Assets/_Scripts/Code425.cs
@@ -5,22 +5,20 @@ using System.Linq;
 public class Code425 : MonoBehaviour {
 	public float maxSpeed = 10;
 	public float explosionRadius = 7f;
-	private Vector3? target;
+	private Vector3 target;
 	[RPC] public void setTarget(Vector3 target) {
 		this.target = target;
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		if (target != null) {
-			Vector3 oldPosition = transform.position;
-			float step = maxSpeed * Time.deltaTime;
-			transform.position = Vector3.MoveTowards(transform.position, target.Value, step);
-			Vector3 newPosition = transform.position;
-			float distanceTravelled = Vector3.Distance(oldPosition, newPosition);
-			if (distanceTravelled < .0001) {
-				Kaboom();
-			}
+		Vector3 oldPosition = transform.position;
+		float step = maxSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+		Vector3 newPosition = transform.position;
+		float distanceTravelled = Vector3.Distance(oldPosition, newPosition);
+		if (distanceTravelled < .0001) {
+			Kaboom();
 		}
 	}
 	
--- Assets/_Scripts/Pellet.cs
+++ Assets/_Scripts/Pellet.cs
@@ -7,22 +7,20 @@ using System.Linq;
 public class Pellet : MonoBehaviour {
 	public float maxSpeed = 30;
 	public float explosionRadius = .1f;
-	private Vector3? target;
+	private Vector3 target;
 	[RPC] public void setTarget(Vector3 target) {
 		this.target = target;
 	}
 
 	// Update is called once per frame
 	void Update() {
-		if (target != null) {
-			Vector3 oldPosition = transform.position;
-			float step = maxSpeed * Time.deltaTime;
-			transform.position = Vector3.MoveTowards(transform.position, target.Value, step);
-			Vector3 newPosition = transform.position;
-			float distanceTravelled = Vector3.Distance(oldPosition, newPosition);
-			if (distanceTravelled < .0001) {
-				Kaboom();
-			}
+		Vector3 oldPosition = transform.position;
+		float step = maxSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+		Vector3 newPosition = transform.position;
+		float distanceTravelled = Vector3.Distance(oldPosition, newPosition);
+		if (distanceTravelled < .0001) {
+			Kaboom();
 		}
 	}
 
