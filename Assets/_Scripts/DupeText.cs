e1632ad81e61130d2d4af18d9a7e5a47d1c19253
Shiliang Zhang <lsftw13@gmail.com>
2015-05-14 23:17:10 -0400

    DupeText.

---
+++ Assets/_Scripts/DupeText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DupeText : MonoBehaviour {
+	public Text original;
+	public Text copy;
+
+	// Update is called once per frame
+	void Update () {
+		copy.text = original.text;
+	}
+}
