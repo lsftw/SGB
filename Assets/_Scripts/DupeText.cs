447b38bb35412b88c9d870e451c8b118c8a8be82
yeealex <yeealex93@gmail.com>
2015-05-14 23:29:02 -0400

    mmm shadows

--- Assets/_Scripts/DupeText.cs
+++ Assets/_Scripts/DupeText.cs
@@ -4,10 +4,16 @@ using System.Collections;
 
 public class DupeText : MonoBehaviour {
 	public Text original;
-	public Text copy;
+	public Text shadowUp;
+	public Text shadowDown;
+	public Text shadowRight;
+	public Text shadowLeft;
 
 	// Update is called once per frame
 	void Update () {
-		copy.text = original.text;
+		shadowUp.text = original.text;
+		shadowDown.text = original.text;
+		shadowRight.text = original.text;
+		shadowLeft.text = original.text;
 	}
 }
